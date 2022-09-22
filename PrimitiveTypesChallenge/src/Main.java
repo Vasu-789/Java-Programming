51ddc72239f59b817f6be41e2feb082490fa7cc8
Vasu-789 <82628986+Vasu-789@users.noreply.github.com>
2022-09-22 20:36:22 +0530

    Delete PrimitiveTypesChallenge directory

--- PrimitiveTypesChallenge/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        byte myByteValue = 120;
-        short myShortValue = 1200;
-        int myIntValue = 12000;
-        long myLongTotal = 50000 + 10 * ( myByteValue + myShortValue + myIntValue );
-        System.out.println("My Long Total Value " + myLongTotal);
-    }
-}
