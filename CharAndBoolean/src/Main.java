ae5008fcfd737465e72e9ec8b38e1624d5686725
Vasu-789 <82628986+Vasu-789@users.noreply.github.com>
2022-09-23 14:03:45 +0530

    Add files via upload

---
+++ CharAndBoolean/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        char myChar = 'V';
+        char myUnicodeChar = '\u0056';
+        System.out.println(myChar);
+        System.out.println(myUnicodeChar);
+        char myNewChar = '\u0C35';
+        System.out.println(myNewChar);
+
+        boolean myTrueBooleanValue = true;
+        boolean myFalseBooleanValue = false;
+
+        boolean isCustomerOverTwenty = true;
+
+    }
+}
