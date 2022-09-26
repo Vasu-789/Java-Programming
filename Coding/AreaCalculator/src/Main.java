bedfa3fe522245ad94a876cfe9481fd9aa95d4b0
Vasu-789 <82628986+Vasu-789@users.noreply.github.com>
2022-09-26 21:48:28 +0530

    Add files via upload

---
+++ Coding/AreaCalculator/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(AreaCalculator.area(5.0d));
+        System.out.println(AreaCalculator.area(-1));
+        System.out.println(AreaCalculator.area(5.0d, 4.0d));
+        System.out.println(AreaCalculator.area(-1, 4.0d));
+    }
+}
