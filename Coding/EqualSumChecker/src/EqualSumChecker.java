e358dc832aaf02cb1b491d3a8a9a2ea7863117c3
Vasu-789 <82628986+Vasu-789@users.noreply.github.com>
2022-09-26 20:14:05 +0530

    Add files via upload

---
+++ Coding/EqualSumChecker/src/EqualSumChecker.java
@@ -0,0 +1,5 @@
+public class EqualSumChecker {
+    public static boolean hasEqualSum(int myNumberOne, int myNumberTwo, int myNumberThree) {
+        return (myNumberOne + myNumberTwo == myNumberThree);
+    }
+}
