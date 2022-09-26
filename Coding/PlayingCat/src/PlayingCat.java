4c5777df5bebc05270ff282a64df25e19ad708b3
Vasu-789 <82628986+Vasu-789@users.noreply.github.com>
2022-09-26 22:01:33 +0530

    Add files via upload

---
+++ Coding/PlayingCat/src/PlayingCat.java
@@ -0,0 +1,11 @@
+public class PlayingCat {
+    public static boolean isCatPlaying(boolean summer, int temperature) {
+        if(temperature < 25 || temperature > 45) {
+            return false;
+        } else if(!summer && temperature <= 35) {
+            return true;
+        } else {
+            return summer;
+        }
+    }
+}
