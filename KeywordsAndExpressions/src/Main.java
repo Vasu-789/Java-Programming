8b1b489610fafdfdc56761e389db7e3bed7dc64c
Vasu-789 <82628986+Vasu-789@users.noreply.github.com>
2022-09-26 11:50:14 +0530

    Add files via upload

---
+++ KeywordsAndExpressions/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        // a mile ios equal to 1.609344 kilometers
+        double kilometers = (100 * 1.609344);
+        int highScore = 50;
+
+        if(highScore == 50){
+            System.out.println("This Is An Expression");
+        }
+
+        //Challenge
+
+        int score = 100; //Expression Is : score = 100
+        if(score > 99){  //Expression Is : score > 99
+            System.out.println("You Got The High Score!"); //Expression Is : "You Got The High Score!"
+            score = 0;   //Expression Is : score = 0
+        }
+    }
+}
