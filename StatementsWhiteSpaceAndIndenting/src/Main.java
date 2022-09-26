09bd71d2f1a846b91a6ebe08a272d20692082d49
Vasu-789 <82628986+Vasu-789@users.noreply.github.com>
2022-09-26 12:30:19 +0530

    Add files via upload

---
+++ StatementsWhiteSpaceAndIndenting/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        int myVariable = 50; //Statement Is : int myVariable = 50;
+        myVariable++;
+        myVariable--;
+        System.out.println("This Is A Test");
+
+        System.out.println("This Is " +
+                "Another " +
+                "Still More.");
+
+        int anotherVariable = 50;
+        myVariable--;
+        System.out.println("This Is Another One");
+    }
+}
