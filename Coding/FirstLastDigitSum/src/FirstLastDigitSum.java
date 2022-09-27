932673b0c6b816bd07e262edcfb14346e923afc4
Vasu-789 <82628986+Vasu-789@users.noreply.github.com>
2022-09-27 22:14:41 +0530

    Add files via upload

---
+++ Coding/FirstLastDigitSum/src/FirstLastDigitSum.java
@@ -0,0 +1,10 @@
+public class FirstLastDigitSum {
+    public static int sumFirstAndLastDigit(int number) {
+        if(number<0) return -1;
+        int sum=number%10;
+        while(number/10>0) {
+            number/=10;
+        }
+        return sum+=number;
+    }
+}
